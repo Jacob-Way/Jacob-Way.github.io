39fae0ac68f2a6bb0ea524e75c3df7c5cbbfc749
Jacob-Way <jd.way23@gmail.com>
2022-11-23 15:49:48 -0600

    equation processor uses svg files

--- latex_updater.py
+++ latex_updater.py
@@ -1,5 +1,5 @@
 import os
-from pdf2image import convert_from_path
+#from pdf2image import convert_from_path
 from hashlib import md5
 
 def update_equations(filepath):
@@ -7,37 +7,23 @@ def update_equations(filepath):
     with open(filepath,"r") as file:
         html = file.read()
     i = 0
+    j=0
     while True:
-        i = html.find("<latex>",i)
-        j = html.find("</latex>",i)
+        i = html.find("<latex>",j)
+        if i==-1:
+            break
+        j = html.find("</latex>",j)
         equation = html[i+7:j]
         print("Latex Equation is: "+equation)
         id = eqhash(equation)
-        print("Hash of this equation is: "+str(id))
-        html = html[:i]+html[j+8:]
-        latex_code =  r"\documentclass[border=0cm]{standalone}\usepackage{tikz}\begin{document}\begin{tikzpicture}\node at (0,0) {\Huge $"+equation+r"$};\end{tikzpicture}\end{document}"
-        latex2png(latex_code,id,"filepath/../equations/")
-        break
-def latex2pdf(latex_markup,filename,folder):
-    f = open(filename+".tex","w")
-    f.write(latex_markup)
-    f.close()
-
-    os.system("pdflatex "+filename+".tex")
-    os.remove(filename+".aux")
-    os.remove(filename+".log")
-    os.remove(filename+".tex")
-    if os.path.exists(folder+filename+".pdf"):
-        os.remove(folder+filename+".pdf")
-    os.rename(filename+".pdf", folder+filename+".pdf")
-def latex2png(latex_markup,filename,folder):
-    latex2pdf(latex_markup,filename,folder)
-    image = convert_from_path(folder+filename+".pdf")
-    image[0].save(folder+filename+".png","PNG")
-    os.remove(folder+filename+".pdf")
+        tex2svg(equation,id,filepath+"/../equations/")
+        replacement_tag = '<img class="latex-inline" src="equations/'+id+'.svg" alt="'+equation+'">'
+        html = html[:i]+replacement_tag+html[j+8:]
+    with open(filepath,"w") as file:
+        file.write(html)
+def tex2svg(latex_markup,id,folder):
+    os.system('tex2svg "'+latex_markup+'" > '+folder+id+'.svg')
 def eqhash(s):
     return md5(s.encode()).hexdigest()[-10:]
-def main():
-    markup = r"\documentclass[border=0cm]{standalone}\usepackage{tikz}\begin{document}\begin{tikzpicture}\node at (0,0) {\Huge $e^{i\pi}=-1$};\end{tikzpicture}\end{document}"
-    latex2png(markup,eqhash(markup),"projects/testproject/equations/")
-update_equations("projects/testproject/test.html")
+update_equations("projects/notes/notes.html")
+
