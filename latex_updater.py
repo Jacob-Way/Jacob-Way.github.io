3c2bc604c216385cb69bd84bc5d29c6437976df0
Jacob-Way <jd.way23@gmail.com>
2022-11-29 22:23:46 -0600

    updating pages

--- latex_updater.py
+++ latex_updater.py
@@ -2,9 +2,9 @@ import os
 #from pdf2image import convert_from_path
 from hashlib import md5
 
-def update_equations(filepath):
+def update_equations(folder,page):
     html = ""
-    with open(filepath,"r") as file:
+    with open(folder+r"/"+page+"_.html","r") as file:
         html = file.read()
     i = 0
     j=0
@@ -14,16 +14,17 @@ def update_equations(filepath):
             break
         j = html.find("</latex>",j)
         equation = html[i+7:j]
-        print("Latex Equation is: "+equation)
         id = eqhash(equation)
-        tex2svg(equation,id,filepath+"/../equations/")
+        if not os.path.exists(folder+"/equations/"+id+".svg"):
+            print("Latex Equation is: "+equation)
+            tex2svg(equation,id,folder+"/equations/")
         replacement_tag = '<img class="latex-inline" src="equations/'+id+'.svg" alt="'+equation+'">'
         html = html[:i]+replacement_tag+html[j+8:]
-    with open(filepath,"w") as file:
+    with open(folder+r"/"+page+".html","w") as file:
         file.write(html)
 def tex2svg(latex_markup,id,folder):
     os.system('tex2svg "'+latex_markup+'" > '+folder+id+'.svg')
 def eqhash(s):
     return md5(s.encode()).hexdigest()[-10:]
-update_equations("projects/notes/notes.html")
+update_equations("projects/nfoldmixing","main")
 
