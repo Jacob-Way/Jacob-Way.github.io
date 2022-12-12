4b80d5a39b59a2375b6ed7fe6de5a395ed347a92
Jacob-Way <jd.way23@gmail.com>
2022-12-11 20:07:38 -0600

    Added pages

--- latex_updater.py
+++ latex_updater.py
@@ -9,22 +9,22 @@ def update_equations(folder,page):
     i = 0
     j=0
     while True:
-        i = html.find("<latex>",j)
+        i = html.find("$",j+1)
         if i==-1:
             break
-        j = html.find("</latex>",j)
-        equation = html[i+7:j]
+        j = html.find("$",i+1)
+        equation = html[i+1:j]
         id = eqhash(equation)
         if not os.path.exists(folder+"/equations/"+id+".svg"):
             print("Latex Equation is: "+equation)
             tex2svg(equation,id,folder+"/equations/")
         replacement_tag = '<img class="latex-inline" src="equations/'+id+'.svg" alt="'+equation+'">'
-        html = html[:i]+replacement_tag+html[j+8:]
+        html = html[:i]+replacement_tag+html[j+1:]
     with open(folder+r"/"+page+".html","w") as file:
         file.write(html)
 def tex2svg(latex_markup,id,folder):
     os.system('tex2svg "'+latex_markup+'" > '+folder+id+'.svg')
 def eqhash(s):
     return md5(s.encode()).hexdigest()[-10:]
-update_equations("projects/nfoldmixing","main")
+update_equations("projects/reviews/hearellipse","main")
 
